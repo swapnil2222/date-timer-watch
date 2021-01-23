f0bcd2b9ca992585d41eef77be49941cd6dfd922
Swapnil <swapnil.renge@geospoc.com>
2021-01-23 23:59:31 +0530

    fix: white list color picker classes using purge css

--- components/Date.vue
+++ components/Date.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="text-3xl" :class="`text-${textClass}`">
+  <div class="text-3xl" :class="textClassName">
     {{ date }}
   </div>
 </template>
@@ -14,6 +14,11 @@ export default {
         day: 'numeric'
       }),
       textClass: ''
+    }
+  },
+  computed: {
+    textClassName () {
+      return `text-${this.textClass}`
     }
   },
   mounted () {
